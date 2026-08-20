e1e75641aacd82ea7f032e226635c17ac7e96c38
Yash <yashgaikwad3295@gmail.com>
2026-08-20 19:47:03 +0530

    chore: configure strict ESLint rules

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(js.configs.recommended, tseslint.configs.recommended, {
+  ignores: ["node_modules/", "dist/", "coverage/", "prisma/migrations/"],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "error",
+  },
+});
